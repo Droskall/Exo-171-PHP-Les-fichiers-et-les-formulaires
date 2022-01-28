48205c042f82b1d6c1aef6e162797342ab0a80f2
DamienOlivier567 <73164252+DamienOlivier567@users.noreply.github.com>
2022-01-28 10:52:16 +0100

    fini

--- index.php
+++ index.php
@@ -14,3 +14,47 @@
  *    Attention, trouvez une solution pour que le fichier contienne du JSON valide !
  * 6. Affichez sur la page index les fichiers ayant déjà été uploadés.
  */
+
+?>
+
+<!doctype html>
+<html lang="fr">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport"
+          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Index</title>
+</head>
+<body>
+<?php
+
+    if (isset($_GET['error'])){
+        $error = $_GET['error'];
+
+        if ($error === "0") {
+            echo "<div>Envoi avec success</div>";
+        }
+        elseif ($error === "La taille du fichier est trop grande (max 3mo)"){
+            echo "<div>" . $error . "</div>";
+        }
+        elseif ($error === "Le type de fichier n'est pas valide") {
+            echo "<div>" . $error . "</div>";
+        }
+        elseif ($error === "Il s'est produit une erreur lors de l'upload") {
+            echo "<div>" . $error . "</div>";
+        }
+    }
+
+?>
+    <div id="container">
+        <form action="fichier.php" method="POST">
+            <div>
+                <label for="file">Votre fichier: </label>
+                <input type="file" id="file" name="file">
+                <input type="submit">
+            </div>
+        </form>
+    </div>
+</body>
+</html>
